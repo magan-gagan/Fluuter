cb6f612627574e69586e8c53faafb2aa0503e356
GAGAN <meraichand@rediffmail.com>
2024-06-27 19:32:04 +0530

    Day 3 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,7 +7,8 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("First App"),
+        title: Text("First App Project"),
+       
       ),
         body: Center(
           child: Container(
